d22ab519d76517bdc2bc2b883ce71ff4590e3398
Reis Haleem <reishaleem@gmail.com>
2020-08-21 16:03:35 -0400

    add bootstrap and set up navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/atoms/Navbar/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+      <Route path="/" component={Navbar} exact />
+        <Route path="/register" component={Navbar} exact />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  
+
+    return (
+        <>
+            <h1>test</h1>
+    
+  
+        </>
+    )
+}
