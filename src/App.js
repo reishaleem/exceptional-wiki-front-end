438dbc4ace22dd9fe56e9e5450d0404d6aca3d9d
Reis Haleem <reishaleem@gmail.com>
2020-08-21 16:06:52 -0400

    add sign up page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/atoms/Navbar/Navbar'
+import SignUp from './components/pages/SignUp/SignUp'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom'
 
@@ -9,7 +10,7 @@ function App() {
     <div>
       <Switch>
       <Route path="/" component={Navbar} exact />
-        <Route path="/register" component={Navbar} exact />
+        <Route path="/register" component={SignUp} exact />
       </Switch>
     </div>
   );
--- src/components/atoms/Navbar/Navbar.jsx
+++ src/components/atoms/Navbar/Navbar.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
 
 export default () => {
 
