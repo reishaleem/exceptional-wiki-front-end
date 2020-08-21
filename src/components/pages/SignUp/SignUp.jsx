27354172486adfadc71ef2ecd9a82e7e58e57750
Reis Haleem <reishaleem@gmail.com>
2020-08-21 19:05:34 -0400

    add proxy

--- src/components/pages/SignUp/SignUp.jsx
+++ src/components/pages/SignUp/SignUp.jsx
@@ -6,7 +6,21 @@ import axios from 'axios'
 async function handleClick(event) {
     event.preventDefault()
     const data = new FormData(event.target)
-    console.log(data.get('username'))
+    data.set("username", data.get("username".toLowerCase()))
+    data.set("email", data.get("email").toLowerCase())
+    console.log(data.get("email"))
+
+    const config = {     
+        headers: { 'content-type': 'multipart/form-data' }
+    }
+
+    axios.post("http://localhost:8080/api/register", data, config)
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        console.log(error)
+    })
 }
 
 export default () => {
@@ -14,7 +28,7 @@ export default () => {
         <Form onSubmit={handleClick}>
             <Form.Group controlId="formBasicName">
                 <Form.Label>Your Name</Form.Label>
-                <Form.Control type="name" placeholder="" />
+                <Form.Control type="name" placeholder="" name="name"/>
             </Form.Group>
 
             <Form.Group controlId="formBasicUsername">
@@ -24,7 +38,7 @@ export default () => {
 
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="" />
+                <Form.Control type="email" placeholder="" name="email"/>
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                 </Form.Text>
@@ -35,12 +49,13 @@ export default () => {
                 <Form.Control
                     type="password"
                     placeholder="At least 6 characters"
+                    name="password"
                 />
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Re-enter Password</Form.Label>
-                <Form.Control type="password" placeholder="" />
+                <Form.Control type="password" placeholder="" name="checkPassword" />
             </Form.Group>
 
             <Button variant="primary" type="submit">
